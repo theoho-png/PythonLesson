79457c27cc170c0b7331cdbd53e6eb1471353602
Theo <62055084+theoho-png@users.noreply.github.com>
2020-07-28 16:06:49 +1000

    Create 03_BuiltInFunctions.py

---
+++ 01-Variable And BuildIn Functions/03_BuiltInFunctions.py
@@ -0,0 +1,24 @@
+#Lesson 01 - Part 03
+#Built-in Functions
+
+#Print Statement
+print("This print statment is working.")
+
+#Input Statement
+print("Please enter your name.")
+name = input()
+
+#Changing data type
+age = int(input("Please enter your age."))
+#!!!The deafult data type for input is String.
+#Therefore, we need to convert the age to int 
+
+#-------------------------------------#
+
+currentYear = 2020
+birthYear = currentYear - age
+
+#Summary
+
+print("Hello " + name +  "\n" + 
+"You are born in " + str(birthYear))
