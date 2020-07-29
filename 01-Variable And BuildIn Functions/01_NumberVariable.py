e7b2b288201817160b8e1de250f3da2a653d011f
Theo <62055084+theoho-png@users.noreply.github.com>
2020-07-29 14:00:04 +1000

    Update 01_NumberVariable.py

--- 01-Variable And BuildIn Functions/01_NumberVariable.py
+++ 01-Variable And BuildIn Functions/01_NumberVariable.py
@@ -1,34 +1,21 @@
-#Lesson 02 - Part 01
-#List
+#Lesson 01 - Part 01
+#Numbers Variable
 
-#Declaring a List
-numberList = [2, 4, 6, 8]
-nameList = ["Michelle", "Nate"]
-theoList = ["Theo", 2000, "October", True]
-print(numberList)
+#Declaring Variable
+myNum = 15 #myNum is an integer
+print(myNum)
 
-#Accessing List Element
-print(nameList[0])
-
-#Changing List Element
-nameList[0] = "Theo"
-print(nameList)
-
-#Adding List Element
-nameList.append("Michelle")
-print(nameList)
+myNum = 20 #Changing value in a variable
+print(myNum)
 
 #-------------------------------------#
 
-subjectList = ["English", "Math", "Science", "IT", "Music"]
-print(subjectList)
+#Multiple Assignment
+a = b = c = 1
+print(a, b, c)
+print(type(a))
 
-#Deleting List Element
-print(subjectList.pop(1)) #Method 1: Pop
-print(subjectList)
-
-del subjectList[0] #Method 2: Del
-print(subjectList)
-
-subjectList.remove("Music") #Method 3: Remove
-print(subjectList)
+a, b, c = 1.0, 2.5, 3 
+#a and b is now a float
+print(type(a))
+print(type(c))
