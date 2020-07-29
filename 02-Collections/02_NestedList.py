9edc4684ce037384137c86706488925e5cfc4d1d
Theo <62055084+theoho-png@users.noreply.github.com>
2020-07-29 13:58:03 +1000

    Create 02_NestedList.py

---
+++ 02-Collections/02_NestedList.py
@@ -0,0 +1,29 @@
+#Lesson 02 - Part 02
+#Neseted List
+
+#Declaring a List
+subjectList = ["English", "Math", "IT", ["Biology", "Chemistry", "Physics"]]
+print(subjectList)
+
+#Accessing Nested List Element
+print(subjectList[3][2])
+
+#Changing List Element
+subjectList[3][2] = "Psychology" #Replaced Physics with Psychology
+print(subjectList)
+
+#Adding Nested List Element
+subjectList[3].append("PE") #Method 1: append
+
+subjectList[3].insert(2,"Physics") #Method 1: insert
+print(subjectList)
+
+#Deleting List Element
+print(subjectList[3].pop(3)) #Method 1: Pop
+print(subjectList)
+
+del subjectList[3][3] #Method 2: Del
+print(subjectList)
+
+subjectList[3].remove("Physics") #Method 3: Remove
+print(subjectList)
