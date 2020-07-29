e1e1675796ecbca1efe1898bc396fa20a6405fba
Theo <62055084+theoho-png@users.noreply.github.com>
2020-07-29 14:39:39 +1000

    Create 03_AccessList.py

---
+++ 02-Collections/03_AccessList.py
@@ -0,0 +1,14 @@
+#Lesson 02 - Part 03
+#More ways to Access a List element
+
+#Declaring a List
+subjectList = ["English", "Math", "IT", "Biology", "Chemistry", "Physics"]
+print(subjectList)
+
+#Accessing List Element
+print(subjectList[3:5])
+
+print(subjectList[3:])
+print(subjectList[:3])
+
+print(subjectList[:])
